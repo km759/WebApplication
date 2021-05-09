ddead5b948dfc9ce4326fa2d2b8fb175b2fde6c1
Luis J Mejia Garcia <ljm36@njit.edu>
2021-05-09 19:55:23 -0400

    Fixed: Everything was fixed. Usernames name show up when they enter the room and leave. Users can send messages to each other.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from typing import List, Dict
 import simplejson as json
-from flask import Flask, request, Response, redirect, url_for
+from flask import Flask, request, Response, redirect
 from flask import render_template
 from flask_cors import CORS
 from flask_socketio import SocketIO, join_room, leave_room
@@ -49,8 +49,16 @@ def session():
 def chat():
     username = request.args.get('username')
     room = request.args.get('room')
-    return render_template('chat.html', username=username, room=room)
+    if username and room:
+        return render_template('chat.html', username=username, room=room)
+    else:
+        return render_template('index.html')
 
+@socketio.on('join_room')
+def handle_join_room_event(data):
+    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
+    join_room(data['room'])
+    socketio.emit('join_room_announcement', data, room=data['room'])
 
 @socketio.on('send_message')
 def handle_send_message_event(data):
@@ -58,13 +66,6 @@ def handle_send_message_event(data):
                                                                     data['room'],
                                                                     data['message']))
     socketio.emit('receive_message', data, room=data['room'])
-
-
-@socketio.on('join_room')
-def handle_join_room_event(data):
-    app.logger.info("{} has joined the room {}".format(data['username'], data['room']))
-    join_room(data['room'])
-    socketio.emit('join_room_announcement', data, room=data['room'])
 
 
 @socketio.on('leave_room')
@@ -194,6 +195,6 @@ def api_delete(oscar_id) -> str:
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    socketio.run(app, host='0.0.0.0', debug=True)
 
 
